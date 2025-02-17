96adb0df08c232ef98362a34f573105df8fdb1f9
Eli Malloy <elimalloy@icloud.com>
2025-02-17 16:21:40 -0500

    update

--- tests.h
+++ tests.h
@@ -1,12 +1,6 @@
 #ifndef TESTS_H
 #define TESTS_H
 
-
-/*
-To be clear, I'm not a TDDTARD, but I'll write the tests I run in here
-so that I can run them again
-*/
-
 #include "parser.h"
 #include <assert.h>
 #include <stdio.h>
