93a1eeaee4d9108f10c2a08cc95950d0d8b95bd2
Eli Malloy <elimalloy@icloud.com>
2025-02-16 20:10:04 -0500

    added definitions

--- intdef.h
+++ intdef.h
@@ -12,5 +12,10 @@ typedef  int16_t  int16;
 typedef  int32_t  int32;
 typedef  int64_t  int64;
 
+typedef uint8_t bool;
+
+// not really ints
+typedef double float64;
+typedef float  float32;
 
 #endif
